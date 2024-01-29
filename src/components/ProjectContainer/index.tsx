bf178f26a6cc90893e21d96255aee6736e2eb790
Murilo Klein <murilo.kkleinn@gmail.com>
2024-01-29 14:48:18 -0300

    Updating

--- src/components/ProjectContainer/index.tsx
+++ src/components/ProjectContainer/index.tsx
@@ -2,30 +2,32 @@ import Link from "next/link";
 import Image from "next/image";
 import GestorImg from "/public/gestorDeEstoqueImg.png";
 import TarefasImg from "/public/listaDeTarefasImg.png";
+import ClimaImg from "/public/siteDoClimaImg.png";
 
 const projectsList = [
   {
     name: "Gestor de Estoque",
     description:
-      "Uma aplicação web SPA (Single Page Application) desenvolvida com HTML, Sass, JavaSript, React, React Router que feita para ajudar no controle de estoque de lojas de varejo, ou outros comércios, com interface bem simples e intuitiva de fácil manejo e entendimento, com armazenamento de dados feito em Local Storage.",
+      "Uma aplicação web SPA (Single Page Application) desenvolvida com HTML, Sass, Bootstrap,  JavaSript, React, React Router que feita para ajudar no controle de estoque de lojas de varejo, ou outros comércios, com interface bem simples e intuitiva de fácil manejo e entendimento, com armazenamento de dados feito em Local Storage.",
     img: GestorImg,
-    techs: ["HTML", "Sass", "JavaScript", "React", "ReactRouter"],
+    techs: ["HTML", "Sass", "JavaScript", "React", "ReactRouter", "Bootstrap"],
     link: "https://stock-manager-gray.vercel.app/",
   },
   {
     name: "Lista de Tarefas",
     description:
-      "Um projeto de uma lista de tarefas semanal SPA (Single Page Application) desenvolvido com HTML, CSS, TypeScript, React e Next.js, feito para o usuário anotar suas atividades semanais de forma simples e intuitiva com armazenamento de dados feito em Local Storage.",
+      "Um projeto de uma lista de tarefas semanal SPA (Single Page Application) desenvolvido com HTML, CSS, Bootstrap, TypeScript, React e Next.js, feito para o usuário anotar suas atividades semanais de forma simples e intuitiva com armazenamento de dados feito em Local Storage.",
     img: TarefasImg,
-    techs: ["HTML", "CSS", "TypeScript", "React", "Next.js"],
+    techs: ["HTML", "CSS", "TypeScript", "React", "Next.js", "Bootstrap"],
     link: "https://to-do-list-ochre-kappa-53.vercel.app/",
   },
   {
-    name: "eee",
-    description: "eee",
-    img: GestorImg,
-    techs: ["eee", "bbb"],
-    link: "eee",
+    name: "Site do Clima",
+    description:
+      "Um Site SPA (Single Page Application) desenvolvido com HTML, Sass, Bootstrap, TypeScript, React, Next.js com para com ajuda da API OpenWeather buscar informações do tempo da cidade que o usuário desejar, com design bem simples e intuitivo.",
+    img: ClimaImg,
+    techs: ["HTML", "Sass", "TypeScript", "React", "Next.js", "Bootstrap"],
+    link: "https://weather-forecast-rose-three.vercel.app/",
   },
 ];
 
